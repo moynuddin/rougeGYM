50c9b7a3fe6d259410a874da27a50087c04cdb16
Moin <moinuddin.mahmood@abbvie.com>
2022-07-04 12:18:39 +0530

    added api key

--- src/exerciseDB/exercise.js
+++ src/exerciseDB/exercise.js
@@ -3,19 +3,11 @@ import axios from "axios";
 export const BASE_URL = "https://exercisedb.p.rapidapi.com";
 
 export const getExercise = () => {
+  console.log("API key", process.env.REACT_APP_API_KEY);
   return axios.get(`${BASE_URL}/exercises`, {
     headers: {
-      "X-RapidAPI-Key": "e70689fba4msh109c4d4b4dd41a1p1a5a7fjsn02fb857922e0",
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
     },
   });
 };
-
-// export const getBodyParts = () => {
-//   return axios.get(`${BASE_URL}/exercises/bodyPartList`, {
-//     headers: {
-//       "X-RapidAPI-Key": "b4b8bc2199msha97402fed16428dp19825ajsn1296671cf107",
-//       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//     },
-//   });
-// };
