3474b983fd6eb9891700f3afd17285a395a49e38
Moin <moinuddin.mahmood@abbvie.com>
2022-07-04 21:55:25 +0530

    api key

--- src/exerciseDB/exercise.js
+++ src/exerciseDB/exercise.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const BASE_URL = "https://exercisedb.p.rapidapi.com";
 
 export const getExercise = () => {
+  console.log("API key", process.env.REACT_APP_API_KEY);
   return axios.get(`${BASE_URL}/exercises`, {
     headers: {
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
