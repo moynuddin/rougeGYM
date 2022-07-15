ef9747a9d9f775cc519a225905136338bed703dc
Moin <moinuddin.mahmood@abbvie.com>
2022-07-16 01:48:15 +0530

    api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.scss";
 import Home from "./Pages/Home";
 function App() {
-  console.log("API key", process.env.REACT_APP_API_KEY);
+  console.log("API key", process.env.REACT_APP_EXERCISE_API_KEY);
+  console.log("API host", process.env.REACT_APP_HOST);
   return <Home />;
 }
 
--- src/exerciseDB/exercise.js
+++ src/exerciseDB/exercise.js
@@ -5,8 +5,8 @@ export const BASE_URL = "https://exercisedb.p.rapidapi.com";
 export const getExercise = () => {
   return axios.get(`${BASE_URL}/exercises`, {
     headers: {
-      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+      "X-RapidAPI-Key": process.env.REACT_APP_EXERCISE_API_KEY,
+      "X-RapidAPI-Host": process.env.REACT_APP_HOST,
     },
   });
 };
